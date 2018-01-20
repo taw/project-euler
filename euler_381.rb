84054a9229fcb551029ebda1383b68b6764a0e9e
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2018-01-20 06:42:44 +0000

    381 good enough solution

--- euler_381.rb
+++ euler_381.rb
@@ -2,27 +2,33 @@
 
 require "prime"
 
-class Integer
-  def bang
-    (1..self).inject(1, &:*)
+def extended_gcd(a, b)
+  last_remainder, remainder = a.abs, b.abs
+  x, last_x, y, last_y = 0, 1, 1, 0
+  while remainder != 0
+    last_remainder, (quotient, remainder) = remainder, last_remainder.divmod(remainder)
+    x, last_x = last_x - quotient*x, x
+    y, last_y = last_y - quotient*y, y
   end
+  return last_remainder, last_x * (a < 0 ? -1 : 1)
+end
 
-  def bang_mod(p)
-    a = 1
-    (2..self).each do |i|
-      a = (a*i) % p
-    end
-    a
+def invmod(e, et)
+  g, x = extended_gcd(e, et)
+  if g != 1
+    raise 'Multiplicative inverse modulo does not exist!'
   end
+  x % et
 end
 
 def s(p)
-  a = (p-5).bang_mod(p)
-  b = a*(p-4)
-  c = b*(p-3)
-  d = c*(p-2)
-  e = d*(p-1)
-  (a+b+c+d+e) % p
+  # Always cancel
+  # e1 = p-1
+  # e2 = 1
+  e3 = invmod(p-2, p)
+  e4 = invmod((p-2)*(p-3) % p, p)
+  e5 = invmod((p-2)*(p-3)*(p-4) % p, p)
+  (e3+e4+e5) % p
 end
 
 def es(maxp)
@@ -37,5 +43,4 @@ end
 
 p s(7)
 p es(100)
-
-p es(20000)
+p es(100_000_000)
